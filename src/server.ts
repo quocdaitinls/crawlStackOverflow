7d2abadbcc9364c1f27e3c343c3b985201106a0b
quocdaitinls <70931328+quocdaitinls@users.noreply.github.com>
2022-03-30 21:42:51 +0700

    add router api-docs

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import apiRouter from "./routes/api";
+import apiDocsRouter from "./routes/api-docs";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use("/api-docs", apiDocsRouter);
 app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
